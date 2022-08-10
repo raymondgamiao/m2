853106fc41836c657f52bf4a715c233afe55e55a
raymondgamiao <royalrfgum@gmail.com>
2022-08-10 17:41:52 +0800

    back to top items

--- js/index.js
+++ js/index.js
@@ -1,9 +1,111 @@
-$(".default_option").click(function(){
-    $(".dropdown ul").addClass("active");
-  });
-  
-  $(".dropdown ul li").click(function(){
-    var text = $(this).text();
-    $(".default_option").text(text);
-    $(".dropdown ul").removeClass("active");
-  });
+let query = "Champion";
+let server = "na1";
+const riotKey = config.riotKey;
+
+async function populate() {
+  let link = "";
+  if (query == "Summoner") {
+    link = `https://${server}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=${riotKey}`;
+  } else if (query == "Item") {
+    link =
+      "http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/item.json";
+  } else {
+    link =
+      "http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/champion.json";
+  }
+
+  const response = await fetch(link);
+  const data = await response.json();
+
+  const searchResult = document.getElementById("infoSearch");
+  searchResult.innerHTML = "";
+
+  if (query == "Summoner") {
+    for (x of data.entries) {
+      searchResult.innerHTML += `<a href="summoners.html?&server=${server}&name=${x.summonerName}"><li class="searchResult">${x.summonerName}</li></a>`;
+    }
+  } else if (query == "Item") {
+    var keyNames = Object.keys(data.data);
+    let count = 0;
+    for (x in data.data) {
+      searchResult.innerHTML += `<a href="insiteItem.html?${keyNames[count]}"><li class="searchResult">${data.data[x].name}</li></a>`;
+      count++;
+    }
+  } else {
+    var keyNames = Object.keys(data.data);
+
+    let count = 0;
+    for (x in data.data) {
+      searchResult.innerHTML += `<a href="insiteChamp.html?${keyNames[count]}"><li class="searchResult">${data.data[x].name}</li></a>`;
+      count++;
+    }
+  }
+}
+populate();
+//code ni matt sa dropdown
+$(".default_option").click(function () {
+  $(".dropdown ul").addClass("active");
+});
+
+$(".dropdown ul li").click(function () {
+  var text = $(this).text();
+  query = text;
+  $(".default_option").text(text);
+  $(".dropdown ul").removeClass("active");
+  populate();
+});
+
+//set active server
+$(".searchRegion button").on("click", function () {
+  $(".searchRegion button").removeClass("selected");
+  $(this).addClass("selected");
+  server = this.value;
+  populate();
+});
+
+function search() {
+  const userInput = document.getElementById("userInput");
+  const query = userInput.value.toUpperCase();
+  const list = document.getElementById("infoSearch");
+  const result = list.querySelectorAll(".searchResult");
+
+  if (query.length > 0) {
+    list.style.display = "";
+  } else {
+    list.style.display = "none";
+  }
+
+  //search all cards for matches with search query
+  //if match then show, else hide
+  for (i = 0; i < result.length; i++) {
+    let champName = result[i].innerText;
+    if (champName.toUpperCase().indexOf(query) > -1) {
+      // summoners[i].style.display = "";
+      result[i].classList.add("visible");
+      result[i].classList.remove("hidden");
+    } else {
+      // summoners[i].style.display = "none";
+      result[i].classList.add("hidden");
+      result[i].classList.remove("visible");
+    }
+  }
+
+  //create no result element
+  let noResult = document.createElement("li");
+  noResult.innerText = "No result";
+  noResult.classList.add("noResult", "hidden");
+  list.append(noResult);
+
+  //search visible
+  const visible = list.querySelectorAll(".visible");
+  const noResultItem = list.querySelector(".noResult");
+
+  //if everything is hidden, show "no result"
+  if (visible.length == 0) {
+    noResultItem.classList.add("visible");
+    noResultItem.classList.remove("hidden");
+  } else {
+    noResultItem.classList.add("hidden");
+    noResultItem.classList.remove("visible");
+  }
+}
--- js/items.js
+++ js/items.js
@@ -167,6 +167,7 @@ function addFilter(filter) {
   //hide mga di pasok sa filter
   getItems();
 }
+
 function removeFilter(filter) {
   const activeFilter = document.getElementById("activeFilter");
   const filters = activeFilter.querySelectorAll(".filter");
@@ -177,3 +178,24 @@ function removeFilter(filter) {
     }
   }
 }
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+calcScrollValue();
